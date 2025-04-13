bbec25830ceb4ff6a3284f67f3edc7641dde2c4e
George Snyder <george@bootstrap.gdsnyder.info>
2025-04-13 17:42:46 -0500

    fix cilium install to use full sha256sum argument name

--- cilium/cilium_client_install.sh
+++ cilium/cilium_client_install.sh
@@ -2,6 +2,6 @@ CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli
 CLI_ARCH=amd64
 if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
 curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz ~/.local/bin
+sha256sum -c cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+tar -xzvf cilium-linux-${CLI_ARCH}.tar.gz -C ~/.local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
